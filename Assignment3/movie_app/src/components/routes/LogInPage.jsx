30314fcd0a65ff1d5e3e1ed6c5e71a231db7819c
tlinhh10102003 <158373100+tlinhh10102003@users.noreply.github.com>
2025-03-27 03:23:20 -0400

    Create LogInPage.jsx for login/ authentication

---
+++ Assignment3/movie_app/src/components/routes/LogInPage.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { useAuth } from '../context/AuthContext';
+
+const LoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const { login } = useAuth();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    login(username, password);
+  };
+
+  return (
+    <div className='login-page'>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Username"
+          required
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          required
+        />
+        <button className='login-button' type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
